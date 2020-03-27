292d213390c2315f73fa5667da198939259856c5
pagotti <pagotti@gmail.com>
2020-03-26 22:51:06 -0300

    Adicionado comandos para observar uma região e para usar o bot no modo inline

--- dasbot/bot.py
+++ dasbot/bot.py
@@ -8,23 +8,52 @@ Modulo bot - Mantem as funções que gerenciam o bot
 import os
 import logging
 import locale
-import pytz
-from datetime import datetime
+import pickle
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from dasbot.corona import GovBR, GovStates, BingData, br_ufs, SeriesChart
+from uuid import uuid4
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent, ParseMode
+from telegram.utils.helpers import escape_markdown
+from dasbot.corona import G1Data, BingData, SeriesChart
 
 
 # Enable logging
 logger = logging.getLogger(__name__)
+admin_id = int(os.environ.get("ADMIN_ID", 0))
 
-corona_br = BingData()
 
+class JobsInfo(object):
+    def __init__(self, file_name):
+        self._jobs = dict()
+        self.file_name = file_name
+        self._props = ["interval", "repeat", "context"]
 
-def check_refresh_data():
-    today = datetime.now().replace(tzinfo=pytz.UTC)
-    if corona_br.last_date is None or corona_br.last_date.replace(tzinfo=pytz.UTC) < today:
-        corona_br.refresh()
+    @property
+    def jobs(self):
+        return self._jobs
+
+    def save(self):
+        with open(self.file_name, 'wb') as f:
+            data = {key: {var: getattr(job, var) for var in self._props} for key, job in self._jobs.items()}
+            pickle.dump(data, f)
+
+    def load(self):
+        if os.path.exists(self.file_name):
+            with open(self.file_name, 'rb') as f:
+                return pickle.load(f)
+        return {}
+
+    def push(self, key, data):
+        self._jobs[key] = data
+
+    def pop(self, key):
+        result = self._jobs[key]
+        del self._jobs[key]
+        return result
+
+    def exists(self, key):
+        return key in self._jobs
 
 
 def start(update, context):
@@ -36,17 +65,16 @@ def start(update, context):
 
 def help(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text("""Comandos que você pode enviar:
+    update.message.reply_text("""Comandos que você pode enviar
     /start : inicia o bot
-    /help : mostra essa ajuda
-    /stats : mostra os números de casos confirmados, suspeitos e descartados
-    Envie uma sigla de estado, por exemplo SC, SP, RJ para saber os dados por estado
-    Outros comandos serão retornados de volta apenas para informar que o bot está funcionando""")
-
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    /help : mostra a ajuda
+    /stats : mostra os números de casos mais atualizados do Brasil
+    /chart : desenha um gráfico com a região informada, ex: /chart SP
+    /listen : observa os dados de uma região a cada X minutos (experimental)
+    /mute : para de observar a região programada com o /listen
+    Envie uma sigla de estado ou nome de cidade, para saber os confirmados nessa região
+    Você pode invocar os dados pelo bot em outros chats:  
+    inicie a mensagem lá com @corona_br_bot e a região na sequência""")
 
 
 def error(update, context):
@@ -56,46 +84,144 @@ def error(update, context):
 
 def stats(update, context):
     logger.info('Arrive /stats command "%s"', update.effective_message)
-    check_refresh_data()
-    update.message.reply_markdown(corona_br.description)
+    sources = [G1Data(), BingData()]
+    result = []
+    for corona in sources:
+        corona.refresh()
+        if corona.last_date:
+            result.append(corona.description)
+    update.message.reply_markdown("\n".join(result))
 
 
 def general(update, context):
-    if context.match:
-        logger.info('Arrive UF message "%s"', update.effective_message)
-        check_refresh_data()
-        uf = context.match.group(0)
-        if uf == "BR":
-            update.message.reply_markdown(corona_br.description)
-        elif uf in br_ufs:
-            corona_states = GovStates(uf)
-            corona_states.refresh()
-            update.message.reply_markdown(corona_states.description)
-        else:
-            update.message.reply_text("UF não reconhecida. Tente novamente.")
+    logger.info('Arrive text message "%s"', update.effective_message)
+    region = update.message.text
+    result = []
+    sources = [G1Data(region), BingData(region)]
+    for corona in sources:
+        corona.refresh()
+        if corona.last_date:
+            result.append(corona.description)
+
+    if result:
+        update.message.reply_markdown("\n".join(result))
     else:
-        echo(update, context)
+        update.message.reply_text("""Região não reconhecida. 
+Envie a sigla do estado em maiúsculas, nomes de cidade com acentos. 
+Envie /help para ver a ajuda.""")
 
 
-def refresh(update, context):
-    # br = GovBR()
-    # br.refresh()
-    # chart = SeriesChart(br)
-    # image = chart.image()
-    # caption = "Atualizado: {}".format(corona_br.last_date.strftime("%d-%m-%Y %H:%M"))
+def _get_chart(regions):
+    sources = []
+    if regions:
+        for region in regions:
+            corona = G1Data(region.strip())
+            corona.refresh()
+            sources.append(corona)
 
-    # context.bot.send_photo("@corona_br", image, caption=caption)
-    # image.seek(0)
-    # msg = corona_br.description
-    # context.bot.send_message("@corona_br", msg, parse_mode="Markdown")
+        chart_br = SeriesChart(*sources)
+        if chart_br.validate():
+            image = chart_br.image()
+            caption = "Atualizado: {}".format(sources[0].last_date.strftime("%d-%m-%Y %H:%M"))
+            return image, caption
+    return None
 
-    update.message.reply_photo(photo=image, caption=caption)
+
+def chart(update, context):
+    logger.info('Arrive /chart command "%s"', update.effective_message)
+    regions = " ".join(context.args).split(",")
+    chart_data = _get_chart(regions)
+    if chart_data:
+        update.message.reply_photo(photo=chart_data[0], caption=chart_data[1])
+    else:
+        update.message.reply_text("""A lista de regiões não foi reconhecida. 
+Envie a sigla de estado em maiúsculas, nomes de cidade com acentos.
+Exemplos: /chart SP, RJ - /chart São Paulo, Belo Horizonte 
+Envie /help para ver a ajuda.""")
 
 
-def channel(update, context):
-    check_refresh_data()
-    msg = corona_br.description
-    context.bot.send_message("@corona_br", msg, parse_mode="Markdown")
+def inline_query(update, context):
+    """Handle the inline query."""
+    query = update.inline_query.query
+    if not query:
+        return
+
+    sources = [G1Data(query), BingData(query)]
+    results = []
+
+    for corona in sources:
+        corona.refresh()
+        if corona.last_date:
+            results.append(InlineQueryResultArticle(
+                id=uuid4(),
+                title="{} por {} em {}".format(query, corona.data_source, corona.last_date.strftime("%d-%m")),
+                input_message_content=InputTextMessageContent(
+                    corona.description,
+                    parse_mode=ParseMode.MARKDOWN)))
+
+    update.inline_query.answer(results, cache_time=60)
+
+
+def unknown(update, context):
+    update.message.reply_text("Não entendi esse comando")
+
+
+def on_change_notifier(context):
+    region = context.job.context["region"]
+    sources = [BingData(region)]
+    for corona in sources:
+        corona.refresh()
+        if corona.last_date:
+            last = context.job.context["last"]
+            if not context.job.context.get("new", True):
+                changes = True
+            else:
+                changes = [1 for i, j in zip(corona.get_data(), last) if i != j]
+            if changes or not last:
+                context.job.context["last"] = corona.get_data()
+                context.bot.send_message(context.job.context["chat_id"], text=corona.description,
+                                         parse_mode=ParseMode.MARKDOWN)
+
+
+_jobs = JobsInfo("logs/jobs.pickle")
+
+
+def set_timer(update, context):
+    """Adiciona uma região na lista de jobs"""
+    chat_id = update.message.chat_id
+    try:
+        region = context.args[0]
+        minutes = int(context.args[1]) if len(context.args) > 1 else 5
+        only_new = context.args[2] == "--new" if len(context.args) > 2 else False
+
+        if not region or minutes < 1:
+            raise ValueError
+
+        # Add job to queue
+        job = context.job_queue.run_repeating(on_change_notifier, minutes * 60, first=5,
+                                              context={"chat_id": chat_id, "region": region, "new": only_new, "last": []})
+        _jobs.push(chat_id, job)
+        _jobs.save()
+
+        update.message.reply_text('Monitoramento ativado!')
+
+    except (IndexError, ValueError):
+        update.message.reply_text("Use: /listen <região> <minutos>\nUse: /mute para parar de observar")
+
+
+def unset_timer(update, context):
+    """Remove o job programado"""
+    chat_id = update.message.chat_id
+
+    if not _jobs.exists(chat_id):
+        update.message.reply_text('Nenhum monitoramento ativo')
+        return
+
+    job = _jobs.pop(chat_id)
+    job.schedule_removal()
+    _jobs.save()
+
+    update.message.reply_text('Monitoramento desativado')
 
 
 def main():
@@ -111,12 +237,28 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("stats", stats))
-    dp.add_handler(CommandHandler("refresh", refresh))
-    dp.add_handler(CommandHandler("channel", channel))
-    dp.add_handler(MessageHandler(Filters.regex(r"^[A-Z]{2}") & ~Filters.update.channel_post, general))
+    dp.add_handler(CommandHandler("chart", chart))
+    dp.add_handler(CommandHandler("listen", set_timer, pass_args=True, pass_job_queue=True))
+    dp.add_handler(CommandHandler("mute", unset_timer))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.update.channel_post, general))
+    dp.add_handler(InlineQueryHandler(inline_query))
+    dp.add_handler(MessageHandler(Filters.command, unknown))
 
-    # echo das mensagens que não são comandos
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.update.channel_post, echo))
+    corona = BingData("BR")
+    corona.refresh()
+    dp.job_queue.run_repeating(on_change_notifier, 300,
+                               context={"chat_id": "@corona_br", "region": "BR", "new": True,
+                                        "last": corona.get_data()})
+
+    # carrega a lista de jobs que estavam programados
+    jobs = _jobs.load()
+    for key, data in jobs.items():
+        if data.get("repeat", True):
+            _jobs.push(key, dp.job_queue.run_repeating(on_change_notifier,
+                                                       data.get("interval", 300), context=data.get("context")))
+        else:
+            _jobs.push(key, dp.job_queue.run_once(on_change_notifier,
+                                                  data.get("interval", 300), context=data.get("context")))
 
     # log all errors
     dp.add_error_handler(error)
@@ -127,4 +269,4 @@ def main():
     # Roda até receber um Ctrl-C
     updater.idle()
 
-
+    _jobs.save()
--- dasbot/corona.py
+++ dasbot/corona.py
@@ -8,9 +8,11 @@ import json
 import re
 import pytz
 import io
+import unicodedata
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+from matplotlib.ticker import MaxNLocator
 from datetime import datetime
 from dateutil import parser
 from urllib.request import urlopen, Request
@@ -48,6 +50,14 @@ br_ufs = {
  'GO': {'uid': '52', 'name': 'Goiás'},
  'DF': {'uid': '53', 'name': 'Distrito Federal'}
 }
+
+
+def _normalize_case(text):
+    return unicodedata.normalize("NFKD", text.casefold())
+
+
+def case_less_eq(left, right):
+    return _normalize_case(left) == _normalize_case(right)
 
 
 def _http_get(url, expected=200):
@@ -147,24 +157,29 @@ class BingData(CoronaData):
         "totalRecovered": "Recuperados"
     }
 
-    def __init__(self):
+    def __init__(self, region=None):
         super().__init__()
         self._data_source = "Bing.com"
-        self._region = "Brasil"
+        self._region = region if region else "BR"
 
     def get_data(self):
         data_keys = ["totalConfirmed", "totalDeaths", "totalRecovered"]
-        return [self._bing.get(k, 0) for k in data_keys]
+        return [self._bing.get(k, 0) or 0 for k in data_keys]
 
     def _get_cases(self):
         cases = []
         for k, v in BingData.categories().items():
-            cases.append("{}: *{:n}*".format(v, self._bing.get(k)))
+            cases.append("{}: *{:n}*".format(v, self._bing.get(k, 0) or 0))
         return ", ".join(cases)
 
     def _update_stats(self):
-        self._last_date = datetime.fromtimestamp(self._version, pytz.timezone("America/Sao_Paulo"))
-        self._bing = [k for k in self._raw_data["areas"] if k["id"] == "brazil"][0]
+        self._bing = [k for k in self._raw_data.get("areas") if k["id"] == "brazil"]
+        if self._region != "BR" and self._bing:
+            region = br_ufs.get(self._region)["name"] if self._region in br_ufs else self._region
+            self._bing = [k for k in self._bing[0].get("areas") if case_less_eq(k["displayName"], region)]
+        if self._bing:
+            self._last_date = datetime.fromtimestamp(self._version, pytz.timezone("America/Sao_Paulo"))
+            self._bing = self._bing[0]
 
     def _load_data(self):
         response = _http_get("https://bing.com/covid/data")
@@ -178,6 +193,87 @@ class BingData(CoronaData):
             return False
 
         with open('bing_cases.json', 'r', encoding='utf-8') as f:
+            self._raw_data = json.load(f)
+        return self._last_date is None
+
+
+class G1Data(CoronaData):
+
+    @staticmethod
+    def categories(): return {
+        "cases": "Confirmados"
+    }
+
+    def __init__(self, region=None):
+        super().__init__()
+        self._data_source = "G1"
+        self._region = region if region else "BR"
+        self._data = None
+        self._match_complete = re.findall(r"([A-zÀ-ú\s]+)[-:\s]*([A-Z]{2})", self._region)
+        self._match_uf = re.findall(r"^[A-Z]{2}$", self._region)
+
+    def _match_region(self, city, state):
+        if self._region == "BR":
+            return True
+        elif self._match_uf:
+            return state == self._match_uf[0]
+        elif self._match_complete:
+            return state == self._match_complete[0][1] and case_less_eq(city, self._match_complete[0][0].strip())
+        else:
+            return case_less_eq(city,  self._region)
+
+    def get_data(self):
+        data_keys = ["cases"]
+        return [self._data.get(k, 0) or 0 for k in data_keys]
+
+    def get_series(self):
+        cases = []
+        for case in self._raw_data["docs"]:
+            if self._match_region(case["city_name"], case["state"]):
+                try:
+                    date = datetime.strptime("{}".format(case.get("date")), "%Y-%m-%d")
+                    acc = cases[-1]["cases"] if len(cases) > 0 else 0
+                    if len(cases) > 0:
+                        acc = cases[-1]["cases"]
+                        if cases[-1]["date"] < date:
+                            cases.append({"date": date, "cases": case.get("cases", 0) + acc})
+                        else:
+                            cases[-1]["cases"] = case.get("cases", 0) + acc
+                    else:
+                        cases.append({"date": date, "cases": case.get("cases", 0)})
+                except ValueError:
+                    pass
+
+        return {c["date"]: [c["cases"], 0, 0] for c in cases}
+
+    def _get_cases(self):
+        cases = []
+        for k, v in G1Data.categories().items():
+            cases.append("{}: *{:n}*".format(v, self._data.get(k, 0) or 0))
+        return ", ".join(cases)
+
+    def _update_stats(self):
+        self._data = {}
+        for case in [k for k in self._raw_data["docs"]]:
+            if self._match_region(case["city_name"], case["state"]):
+                for k in G1Data.categories():
+                    self._data[k] = self._data.get(k, 0) + case.get(k, 0)
+        self._last_date = datetime.fromtimestamp(self._version, pytz.timezone("America/Sao_Paulo")) \
+            if self._data else None
+
+    def _load_data(self):
+        response = _http_get("https://especiais.g1.globo.com/bemestar/coronavirus/mapa-coronavirus/data/brazil-cases.json")
+        if response:
+            data = json.loads(response.read())
+            date = re.findall(r"(\d{1,2})/(\d{1,2})/(\d{4}), às (\d{1,2}:\d{1,2})", data["updated_at"])[0]
+            version = parser.parse("{}-{}-{} {}".format(date[2], date[1], date[0], date[3])).timestamp()
+            if version != self._version:
+                self._version = version
+                self._raw_data = data
+                return True
+            return False
+
+        with open('g1_cases.json', 'r', encoding='utf-8') as f:
             self._raw_data = json.load(f)
         return self._last_date is None
 
@@ -268,35 +364,68 @@ class GovStates(GovBR):
 
 class SeriesChart(object):
 
-    def __init__(self, corona_data: CoronaData):
-        self.source = corona_data
+    def __init__(self, *args):
+        self.series = []
+        self.regions = []
+        self.source = args[0].data_source
+        for corona in args:
+            self.series.append(corona.get_series())
+            self.regions.append(corona.region)
+
+    def validate(self):
+        for series in self.series:
+            if not series:
+                return False
+        return True
 
     def image(self):
-        categories = {
-            0: "Confirmados",
-            1: "Mortos",
-            2: "Recuperados"
-        }
-
         x_axis = []
         y_axis = {}
-        for k in categories:
-            y_axis[k] = []
-
-        for date, values in self.source.get_series().items():
-            x_axis.append(date)
-            for k in categories:
-                y_axis[k].append(values[k])
 
         fig = plt.figure(figsize=(10, 5))
-        for k, v in categories.items():
-            plt.plot(x_axis, y_axis[k], label=v)
+
+        if len(self.series) == 1:
+            categories = {
+                0: "Confirmados"
+                # 1: "Mortos",
+                # 2: "Recuperados"
+            }
+            for k in categories:
+                y_axis[k] = []
+            series = self.series[0]
+            for date, values in series.items():
+                x_axis.append(date)
+                for k in categories:
+                    y_axis[k].append(values[k])
+            for k, v in categories.items():
+                plt.plot(x_axis, y_axis[k], label=v)
+            plt.title("Contaminação pelo COVID-19 : {} - Fonte: {}".format(self.regions[0], self.source))
+        else:
+            dates = {}
+            for i, series in enumerate(self.series):
+                y_axis[i] = []
+                for date in series:
+                    dates[date] = None
+            x_axis = [k for k in dates.keys()]
+            x_axis.sort()
+            for i, series in enumerate(self.series):
+                for date in x_axis:
+                    if date in series:
+                        y_axis[i].append(series[date][0])
+                    else:
+                        if len(y_axis[i]) > 0:
+                            y_axis[i].append(y_axis[i][-1])
+                        else:
+                            y_axis[i].append(0)
+                plt.plot(x_axis, y_axis[i], label=self.regions[i])
+            plt.title("Contaminação pelo COVID-19 : Confirmados - Fonte: {}".format(self.source))
+
         ax = plt.gca()
         ax.xaxis.set_major_formatter(mdates.DateFormatter("%d-%b"))
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
         plt.xlabel('Data')
         plt.ylabel('Quantidade')
-        plt.title("Contaminação pelo COVID-19 : {}".format(self.source.region))
         plt.legend()
 
         file = io.BytesIO()
